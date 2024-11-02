b60e416419f21e7c75a58557b9ca463ff1f4bff5
manicness <michael@manicness.com>
2024-11-02 16:07:55 +0000

    Date formatter.

---
+++ src/scripts/utilities.ts
@@ -0,0 +1,13 @@
+// Format the date to a string
+export function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'short', day: 'numeric'};
+  
+    return new Date(date).toLocaleDateString(undefined, options);
+  }
+  // Capitalize the first letter
+export function capitalize(str:string): string {
+  if ( typeof str !== 'string' || str.length === 0 ) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
